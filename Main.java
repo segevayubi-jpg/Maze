d490bf09a392cec697c163e2af4ccfc144f75fac
oriya200912-a11y <oriya200912@gmail.com>
2026-09-02 16:42:44 +0300

    Update Main.java

--- Main.java
+++ Main.java
@@ -4,14 +4,10 @@ public class Main {
 
     public static void main(String[] args) {
 
-        ApiClient apiClient = new ApiClient();
-
-        String config = apiClient.getRenderConfig();
-
-        System.out.println(config);
-
         SwingUtilities.invokeLater(() -> {
+
             MainFrame frame = new MainFrame();
+
             frame.setVisible(true);
         });
     }
