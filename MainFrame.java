f1f13c71d1922a6a4ce30d83628d1dc9bf818a4a
oriya200912-a11y <oriya200912@gmail.com>
2026-09-02 22:43:01 +0300

    Update MainFrame.java

--- MainFrame.java
+++ MainFrame.java
@@ -67,32 +67,7 @@ public class MainFrame extends JFrame {
             }
         }
     }
-    private void readMazePixels() {
-    
-        if (mazeImage == null) {
-            return;
-        }
-    
-        int imageWidth = mazeImage.getWidth();
-        int imageHeight = mazeImage.getHeight();
-    
-        for (int y = 0; y < imageHeight; y++) {
-    
-            for (int x = 0; x < imageWidth; x++) {
-    
-                int rgb = mazeImage.getRGB(x, y);
-    
-                Color pixelColor = new Color(rgb);
-    
-                System.out.println(
-                        "Pixel (" + x + "," + y + ")"
-                                + " R=" + pixelColor.getRed()
-                                + " G=" + pixelColor.getGreen()
-                                + " B=" + pixelColor.getBlue()
-                );
-            }
-        }
-    }
+
     private void handleGetMaze() {
     
         readMazeSize();
