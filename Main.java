48ce48c6d70cfc9b3cc7d93482bb242641ba7851
oriya200912-a11y <oriya200912@gmail.com>
2026-09-02 15:40:27 +0300

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,1 +1,13 @@
+import javax.swing.SwingUtilities;
 
+public class Main {
+
+    public static void main(String[] args) {
+
+        SwingUtilities.invokeLater(() -> {
+            MainFrame frame = new MainFrame();
+            frame.setVisible(true);
+        });
+
+    }
+}
