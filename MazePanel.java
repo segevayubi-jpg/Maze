b0279a85829a794d2ddf2e26271a0b239ed8d28f
oriya200912-a11y <oriya200912@gmail.com>
2026-09-04 15:20:38 +0300

    Update MazePanel.java

--- MazePanel.java
+++ MazePanel.java
@@ -1,1 +1,77 @@
+import javax.swing.*;
+import java.awt.*;
 
+public class MazePanel extends JPanel {
+
+    private boolean[][] maze;
+    private RenderConfig renderConfig;
+    private int cellSize;
+
+    public MazePanel(boolean[][] maze,
+                     RenderConfig renderConfig,
+                     int cellSize) {
+
+        this.maze = maze;
+        this.renderConfig = renderConfig;
+        this.cellSize = cellSize;
+
+        int width = maze[0].length * cellSize;
+        int height = maze.length * cellSize;
+
+        setPreferredSize(
+                new Dimension(width, height)
+        );
+    }
+
+    @Override
+    protected void paintComponent(Graphics g) {
+
+        super.paintComponent(g);
+
+        Color wallColor =
+                Color.decode(
+                        renderConfig.getWallCellColor()
+                );
+
+        Color gridColor =
+                Color.decode(
+                        renderConfig.getGridColor()
+                );
+
+        for (int y = 0; y < maze.length; y++) {
+
+            for (int x = 0; x < maze[y].length; x++) {
+
+                if (maze[y][x]) {
+
+                    // true = passage
+                    g.setColor(Color.WHITE);
+
+                } else {
+
+                    // false = wall
+                    g.setColor(wallColor);
+                }
+
+                g.fillRect(
+                        x * cellSize,
+                        y * cellSize,
+                        cellSize,
+                        cellSize
+                );
+
+                if (renderConfig.isDrawGrid()) {
+
+                    g.setColor(gridColor);
+
+                    g.drawRect(
+                            x * cellSize,
+                            y * cellSize,
+                            cellSize,
+                            cellSize
+                    );
+                }
+            }
+        }
+    }
+}
